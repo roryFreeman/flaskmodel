91f6493e767225926c32e59afdabbda002874c34
Rory Freeman <925777@uk.ema.ad.pwcinternal.com>
2019-08-21 23:15:11 +0100

    with debug 2

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def model_predict():
         return "Must post json"                  
             
 if __name__ == "__main__":        
-    app.run()                    
+    app.run(debug=True)                    
